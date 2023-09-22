3bd85aa6dbaffc74865aa9044c919b5f4bd6978d
Patrick Macedo <144183578+ptkmcd@users.noreply.github.com>
2023-09-22 15:01:07 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,5 @@
+// Solicita ao usuário que insira um salário
+const salario = prompt("Por favor, insira o seu salário:");
+
+// Exibe um alerta com o salário inserido pelo usuário
+alert(`Você informou o seguinte salário: R$ ${salario}`);
